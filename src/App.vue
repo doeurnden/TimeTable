cb7137eb4cba3451b54e62b9e42a13db2c86ec87
doeurnden32 <doeurnden32@gmail.com>
2023-08-22 14:59:41 +0700

    checking code

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import LeftSideBar from './components/LeftSideBar.vue';
+import LeftSideBar from './components/LeftSideBar.vue'
 import FullCalendar_Middle from './components/FullCalendar_Middle.vue'
 import RightSideBar from './components/RightSideBar.vue';
 
@@ -19,24 +19,3 @@ export default {
   }
 };
 </script>
-<<<<<<< HEAD
-=======
-<style>
-    *{
-        margin: 0px;
-        padding: 0px;
-        box-sizing: border-box;
-    }
-    body{
-        width: 100%;
-        height: 100vh;
-        /* background-color: green; */
-    }
-    .Full-Calendar{
-        width: 100%;
-        height: 100vh;
-        /* background-color: blue; */
-        display: flex;
-    }
-</style>
->>>>>>> 3d5897b99b37c6e3102c1a6eb69d3edb819fba92
