714486dc8159bfaae2b4f07cdc24c7db0b45e2f3
doeurnden32 <doeurnden32@gmail.com>
2023-08-23 15:23:35 +0700

    completed course button feature

--- src/components/RightSideBar.vue
+++ src/components/RightSideBar.vue
@@ -1,22 +1,20 @@
 <template>
     <div class="contianer">
         <nav class="navbar">
-            <div class="course">
-                <p><i class="icon pi pi-book"></i></p>
-                <p>course</p>
-            </div>
-            <div class="room">
-                <p><i class="icon pi pi-building"></i></p>
-                <p>room</p>
-            </div>
-            <div class="lecturer">
-                <p><i class="icon pi pi-users"></i></p>
-                <p>lecturer</p>
-            </div>
+            <button class="navbars course">
+                <span><i class="icon pi pi-book"></i>Course</span>
+            </button>
+            <button class="navbars room">
+                <span><i class="icon pi pi-building"></i>Room</span>
+            </button>
+            <button class="navbars lecturer">
+                <span><i class="icon pi pi-users"></i>Lecturer</span>
+            </button>
         </nav>
         <div class="sub-container">
             <div class="search">
                 <input type="text" placeholder="search subject">
+                <i class="icon pi pi-search"></i>
             </div>
             <div class="data">
                 <div class="items">
@@ -27,7 +25,7 @@
                         </div>
                         <div class="course-info">
                             <div>Internet Programming</div>
-                            <div><button>Unasigned</button></div>
+                            <div class="btn"><button>Unsigned</button></div>
                             <div>TP : 16 H</div>
                         </div>
                     </div>
@@ -39,10 +37,152 @@
                         </div>
                         <div class="course-info">
                             <div>Internet Programming</div>
-                            <div><button>Unasigned</button></div>
+                            <div class="btn"><button>Unsigned</button></div>
                             <div> TP : 16 H</div>
                         </div>
                     </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div>TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div> TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div>TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div> TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div>TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button >Unsigned</button></div>
+                            <div> TP : 16 H</div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div>TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div> TP : 16 H</div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div>TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div> TP : 16 H</div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div>TP : 16 H</div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="icons">
+                            <i class="icon pi pi-ellipsis-v"></i>
+                            <i class="icon pi pi-ellipsis-v"></i>
+                        </div>
+                        <div class="course-info">
+                            <div>Internet Programming</div>
+                            <div class="btn"><button>Unsigned</button></div>
+                            <div> TP : 16 H</div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -54,53 +194,95 @@ import 'primeicons/primeicons.css';
 </script>
 
 <style scoped>
+/* container */
 .contianer {
     width: 20%;
     height: 100vh;
-    background-color: rgb(190, 190, 190);
+    /* background-color: rgb(190, 190, 190); */
+    font-family: Arial, Helvetica, sans-serif;
 }
+/* navbar */
 .contianer .navbar{
     width: 100%;
-    height: 10%;
+    height: 6%;
     display: flex;
-    justify-content: space-around;
-    align-items: center;
-    background-color: antiquewhite;
+    justify-content: space-between;
+    align-items:end;
+    /* background-color: antiquewhite; */
+    padding-bottom: 5px;
+    /* padding-left: 5%; */
 }
-.contianer .navbar .course, .room, .lecturer{
-    display: flex;
-    gap: .5rem;
+.contianer .navbar .navbars{
+    border: 1px solid black;
+}:hover{
+    cursor: pointer;
 }
+.contianer .navbar .navbars{
+    width: 30%;
+    height: 70%;
+    border: none;
+    border-radius: 5px;
+    background-color: #3aa6b9;
+    color: white;
+}
+.contianer .navbar .navbars:hover{
+    background-color: #0c356a;
+}
+.contianer .navbar .navbars span .icon{
+    padding-right: .5rem;
+}
+/* .sub-container */
 .contianer .sub-container{
     width: 100%;
-    height: 90%;
+    height: 95%;
     /* background-color: aqua; */
 }
+/* .search */
 .contianer .sub-container .search{
     width: 100%;
-    height: 10%;
-    background-color: aqua;
+    height: 5%;
+    /* background-color: aqua; */
     display: flex;
     justify-content: center;
     align-items: center;
+    border: 1px solid black;
+    margin: .5rem 0;
 }
 .contianer .sub-container .search input{
-    width: 80%;
+    width: 90%;
+    padding: .3rem;
+    outline: none;
+    position: relative;
+}
+.contianer .sub-container .search .icon{
+    position: absolute;
+    right: 2rem;
 }
 .contianer .sub-container .data{
     width: 100%;
-    height: 90%;
+    height: 92.2%;
+    background-color: rgb(255, 255, 255);
+    border: 1px solid black;
 }
 .contianer .sub-container .data .items{
-    width: 100%;
-    height: 100%;
-    /* padding: 1rem; */
-    /* margin: 1rem; */
+    width: 90%;
+    height: 95%;
+    background-color: #ece8e8;
+    /* padding: 5%; */
+    margin: 5%;
+    overflow-y: scroll;
 }
 .contianer .sub-container .data .items .item{
     height: 15%;
-    width: 100%;
-    background-color: rgb(13, 0, 255);
+    /* width: 100%; */
+    background-color: #ffffff;
+    /* padding: 5%; */
+    margin: 5%;
+    /* margin-top: 10%; */
+    border-left: 2px solid #ff0000;
+}
+.contianer .sub-container .data .items .item:hover{
+   cursor: pointer;
 }
 .contianer .sub-container .data .items .item .icons{
     height: 30%;
@@ -110,6 +292,7 @@ import 'primeicons/primeicons.css';
     margin: 0; */
     display: flex;
     align-items: center;
+    padding-left: .5rem;
 }
 .contianer .sub-container .data .items .item .course-info{
     height: 70%;
@@ -118,5 +301,13 @@ import 'primeicons/primeicons.css';
     display: flex;
     flex-direction: column;
     justify-content: space-around;
+    padding-left: 5%;
+}
+.contianer .sub-container .data .items .item .course-info .btn button{
+    color: white;
+    background-color: #ff0000;
+    border: none;
+    border-radius: 5px;
+    padding: .2rem;
 }
 </style>
