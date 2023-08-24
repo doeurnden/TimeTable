1dcd8e8df42737314c060011b2f4c014efa03e2f
thearith99 <athoudomthearith.com>
2023-08-24 13:51:40 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -20,3 +20,23 @@
     }
   };
 </script>
+
+<style scoped>
+
+*{
+      margin: 0px;
+      padding: 0px;
+      box-sizing: border-box;
+    }
+    body{
+      width: 100%;
+      height: 100vh;
+      /* background-color: green; */
+    }
+    .container{
+      width: 100%;
+      height: 100%;
+      display: flex;
+    }
+
+</style>
