9fd9413d611255c4cf486751de8b5a29f4598075
doeurnden32 <doeurnden32@gmail.com>
2023-08-24 13:57:30 +0700

    modify code

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="container">
-      <LeftSideBar />
-      <FullCalendar_Middle/>
-      <RightSideBar/>
-    </div>
+  <div class="container">
+    <LeftSideBar />
+    <FullCalendar_Middle />
+    <RightSideBar />
+  </div>
 </template>
   
 <script>
-  import LeftSideBar from './components/LeftSideBar.vue'
-  import FullCalendar_Middle from './components/FullCalendarMiddle.vue'
-  import RightSideBar from './components/RightSideBar.vue';
-  // import timeGridPlugin from '@fullcalendar/timegrid'
-  
-  export default {
-    components: {
-      LeftSideBar,
-      FullCalendar_Middle,
-      RightSideBar
-    }
-  };
+import LeftSideBar from './components/LeftSideBar.vue'
+import FullCalendar_Middle from './components/FullCalendarMiddle.vue'
+import RightSideBar from './components/RightSideBar.vue';
+// import timeGridPlugin from '@fullcalendar/timegrid'
+
+export default {
+  components: {
+    LeftSideBar,
+    FullCalendar_Middle,
+    RightSideBar
+  }
+};
 </script>
 
 <style scoped>
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
 
-*{
-      margin: 0px;
-      padding: 0px;
-      box-sizing: border-box;
-    }
-    body{
-      width: 100%;
-      height: 100vh;
-      /* background-color: green; */
-    }
-    .container{
-      width: 100%;
-      height: 100%;
-      display: flex;
-    }
+body {
+  width: 100%;
+  height: 100vh;
+  /* background-color: green; */
+}
 
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
 </style>
