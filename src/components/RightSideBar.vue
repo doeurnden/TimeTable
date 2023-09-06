542657ee53256c4957ac19a8e4ad3216e729cdb2
doeurnden32 <doeurnden32@gmail.com>
2023-09-06 16:46:16 +0700

    check connection between left side, right side and fullcalender

--- src/components/RightSideBar.vue
+++ src/components/RightSideBar.vue
@@ -167,25 +167,24 @@ export default {
         }
     },
     mounted() {
-        axios.get(`http://127.0.0.1:8000/api/rooms/get_rooms`)
-            .then(response => {
+        axios.get(import.meta.env.VITE_APP_ROOM)
+             .then(response => {
                 this.rooms = response.data;
-            })
-            .catch(error => {
-                console.error('An error occurred:', error);
-            });
+             })
         // axios.get(`http://127.0.0.1:8000/api/employees/get_lecturer`)
         //     .then(response => {
         //         this.lecturers = response.data.data;
         //     });
-        axios.get(`http://127.0.0.1:8000/api/course/get_course`)
+        axios.get(import.meta.env.VITE_APP_COURSE)
             .then(response => this.courses = response.data);
 
-        axios.get(`http://127.0.0.1:8000/api/employees/get_lecturer`)
+        axios.get(import.meta.env.VITE_APP_LECTURER)
             .then(response => {
                 this.lecturers = response.data;
             });
-
+//move from fullcalender center
+        this.fetchAcademyYears()
+        this.selectedAcademyYear = this.selectedAcademyYear ?? this.fetchedAcademyYears[0]
 
     },
     computed: {
