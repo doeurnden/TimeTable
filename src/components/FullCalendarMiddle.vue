1beaccaa2fe2937b39b5153350bed7a6fe85cf0d
Monirom <monirom094@.com>
2023-09-19 09:19:24 +0700

    change

--- src/components/FullCalendarMiddle.vue
+++ src/components/FullCalendarMiddle.vue
@@ -218,25 +218,30 @@ export default {
             const day = date.toLocaleDateString('en-US', { weekday: 'long' }); // Change 'long' to 'short' if you prefer abbreviated names
             return day;
         },
-        handleEventClick(eventClickInfo) {
-            // This function will be called when an event is clicked
-            const event = eventClickInfo.event;
-            console.log('Event clicked:', event.title, event.start);
-        },
+        // handleEventClick(eventClickInfo) {
+        //     // This function will be called when an event is clicked
+        //     const event = eventClickInfo.event;
+        //     console.log('Event clicked:', event.title, event.start);
+        // },
+        // @prevent-slot-from-time-11&12
         handleEventAllow(dropInfo) {
-            // Prevent dropping events in the specified time range (11am to 12pm)
             const startTime = dropInfo.start;
             const endTime = dropInfo.end;
+
+            // Define the allowed time slots
+            const allowedStartTime = new Date(startTime);
+            allowedStartTime.setHours(11, 0, 0, 0); // 12:00:00 PM
+            const allowedEndTime = new Date(startTime);
+            allowedEndTime.setHours(13, 0, 0, 0); // 1:00:00 PM
+
+            // Check if the event falls entirely within the allowed time slots
             if (
-                startTime.getHours() >= 11 &&
-                // endTime.getHours() >= 12 &&
-                startTime.getHours() <= 12
-                // endTime.getHours() >= 12
+                endTime <= allowedStartTime || // Event ends before 12pm
+                startTime >= allowedEndTime     // Event starts after 1pm
             ) {
-                return false; // Disallow dropping events
-            }
-            else {
                 return true; // Allow dropping events
+            } else {
+                return false; // Disallow dropping events
             }
         },
         customEventContent(eventInfo) {
@@ -385,383 +390,384 @@ export default {
 </script>
 
 <style>
-.FullCalendar_Middle {
-    width: 65%;
-    height: 100vh;
-    /* background-color: blueviolet; */
-    font-family: Arial, Helvetica, sans-serif;
-    position: relative;
-    padding-bottom: 0px;
-}
-
-.fullcalendar {
-    margin-top: 57px;
-    padding-left: 10px;
-    padding-right: 10px;
-}
-
-.title-content {
-    position: absolute;
-    top: 19px;
-}
-
-.title-content h1 {
-    font-size: 28px;
-}
-
-.hori {
-    position: relative;
-    width: 100%;
-    height: 5px;
-    overflow: hidden;
-}
-
-.hori hr {
-    width: 100%;
-    height: 5px;
-    color: #3AA6B9;
-    background-color: #3AA6B9;
-    position: absolute;
-    border: none;
-    animation: slideFromLeft 6s linear forwards;
-}
-
-@keyframes slideFromLeft {
-    0% {
-        left: -100%;
+    .FullCalendar_Middle {
+        width: 65%;
+        height: 100vh;
+        /* background-color: blueviolet; */
+        font-family: Arial, Helvetica, sans-serif;
+        position: relative;
+        padding-bottom: 0px;
     }
 
-    100% {
-        left: 0;
+    .fullcalendar {
+        margin-top: 57px;
+        padding-left: 10px;
+        padding-right: 10px;
     }
-}
 
-.select-group-week {
-    width: 200px;
-    height: 35px;
-    /* background-color: green; */
-    position: absolute;
-    top: 17px;
-    right: 8px;
-    display: flex;
-    justify-content: space-between;
-}
+    .title-content {
+        position: absolute;
+        top: 19px;
+    }
 
-.select-group-week .select-group {
-    width: 96px;
-    height: 35px;
-    /* background-color: blue; */
-}
+    .title-content h1 {
+        font-size: 28px;
+    }
 
-.select-group-week .select-group select {
-    width: 100%;
-    height: 35px;
-    border-radius: 5px;
-    background-color: #3AA6B9;
-    color: white;
-    border: none;
-    outline: 0px;
-    font-size: 15px;
-    padding: 3px;
-    transition: all 0.5s;
-}
-
-.select-group-week .select-week {
-    width: 96px;
-    height: 100%;
-    /* background-color: black; */
-}
-
-.select-group-week .select-week select {
-    width: 100%;
-    height: 35px;
-    border-radius: 5px;
-    background-color: #3AA6B9;
-    color: white;
-    border: none;
-    outline: 0px;
-    font-size: 15px;
-    padding: 3px;
-    transition: all 0.6s;
-}
-
-.select-group-week .select-group select:hover,
-.select-week select:hover {
-    cursor: pointer;
-    background-color: #0C356A;
-    transition: all 0.7s;
-}
-
-/* @change-slot-time-height */
-td.fc-timegrid-slot {
-    height: 37px !important;
-}
-
-.fc .fc-toolbar.fc-header-toolbar {
-    margin-bottom: 0.54em;
-}
-
-/* @turn-off-color-current-day */
-.fc .fc-timegrid-col.fc-day-today {
-    background-color: white;
-}
-
-/* @color-11-12pm */
-tbody tr:nth-child(9),
-tbody tr:nth-child(10),
-tbody tr:nth-child(11),
-tbody tr:nth-child(12),
-tbody tr:nth-child(21) {
-    background-color: #f4f4f4 !important;
-}
-
-/* @like-colespan on 11 & 12pm */
-.fc .fc-non-business {
-    background-color: transparent;
-}
-
-a.fc-event,
-a.fc-event {
-    background-color: #3AA6B9;
-    border: 1px solid #3AA6B9;
-}
-
-.fc-timegrid-event .fc-event-main {
-    padding: 0px;
-}
-/* @event */
-.container-room {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    background-color: #3AA6B9;
-    position: relative;
-}
-.container-room:hover .delete{
-    width: 13px;
-    height: 13px;
-    background-color: red;
-    border-top-right-radius: 3px;
-    color: white;
-    font-weight: bold;
-    position: absolute;
-    text-align: center;
-    justify-content: center;
-    z-index: 1;
-    right: 0;
-    top: 0;
-    font-size: 10px;
-    cursor: pointer;
-}
-.container-room:hover .sideCourse{
-    background-color: #FFF6E0 !important;
-}
-
-/* @course-section */
-.sideCourse {
-    width: 100%;
-    height: 63%;
-    background-color: white;
-    padding: 6px 6px;
-    font-size: 13px;
-    position: absolute;
-    font-weight: 600;
-    border-top-left-radius: 3px;
-    border-top-right-radius: 3px;
-}
-.courseName .courseNameText {
-    font-size: 14px;
-    color: black;
-    /* margin-bottom: 6px;
-        line-height: 1.3em;
-        max-height: calc(2 * 1.2em);
+    .hori {
+        position: relative;
+        width: 100%;
+        height: 5px;
         overflow: hidden;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2;  */
-}
+    }
 
-.courseType {
-    padding-bottom: 6px;
-    color: red;
-}
+    .hori hr {
+        width: 100%;
+        height: 5px;
+        color: #3AA6B9;
+        background-color: #3AA6B9;
+        position: absolute;
+        border: none;
+        animation: slideFromLeft 6s linear forwards;
+    }
 
-.lecturer {
-    color: green;
-}
+    @keyframes slideFromLeft {
+        0% {
+            left: -100%;
+        }
 
-/* @room-section */
-.sideRoom {
-    width: 100%;
-    height: 37%;
-    background-color: #3AA6B9;
-    bottom: 0;
-    position: absolute;
-    padding: 6px 6px;
-    font-weight: 500;
-    font-size: 13px;
-}
+        100% {
+            left: 0;
+        }
+    }
 
-.roomName {
-    padding-bottom: 4px;
-}
-.delete h1{
-    width: 13px;
-    height: 13px;
-    display: none;
-    transition: all 0.5s;
-}
-.container-room:hover .delete{
-    width: 13px;
-    height: 13px;
-    background-color: red;
-    border-top-right-radius: 3px;
-    color: white;
-    font-weight: bold;
-    position: absolute;
-    text-align: center;
-    justify-content: center;
-    z-index: 1;
-    right: 0;
-    top: 0;
-    font-size: 10px;
-    cursor: pointer;
-    transition: all 0.5s;
-}
-/* 
-.delete {
-    width: 13px;
-    height: 13px;
-    background-color: red;
-    border-top-right-radius: 3px;
-    color: white;
-    font-weight: bold;
-    position: absolute;
-    text-align: center;
-    justify-content: center;
-    z-index: 1;
-    right: 0;
-    top: 0;
-    font-size: 10px;
-    cursor: pointer;
-} */
+    .select-group-week {
+        width: 200px;
+        height: 35px;
+        /* background-color: green; */
+        position: absolute;
+        top: 17px;
+        right: 8px;
+        display: flex;
+        justify-content: space-between;
+    }
 
-.swal2-container .swal2-popup {
-    width: 25rem;
-    height: 270px;
-    max-width: 100%;
-    border: none;
-    border-radius: 5px;
-    background: #fff;
-    color: #545454;
-    display: none;
-    padding: 0 0.1em 2.3em;
-    position: relative;
-    box-sizing: border-box;
-    grid-template-columns: minmax(0, 100%);
-    font-family: inherit;
-    font-size: 1rem;
-}
+    .select-group-week .select-group {
+        width: 96px;
+        height: 35px;
+        /* background-color: blue; */
+    }
 
-.swal2-icon.swal2-warning {
-    border-color: red;
-    color: red;
-    margin-bottom: -25px;
-    /* font-size: 14px; */
-}
+    .select-group-week .select-group select {
+        width: 100%;
+        height: 35px;
+        border-radius: 5px;
+        background-color: #3AA6B9;
+        color: white;
+        border: none;
+        outline: 0px;
+        font-size: 15px;
+        padding: 3px;
+        transition: all 0.5s;
+    }
 
-.swal2-title {
-    /* font-size: 25px; */
-    color: black;
-    font-family: Arial, Helvetica, sans-serif;
-}
+    .select-group-week .select-week {
+        width: 96px;
+        height: 100%;
+        /* background-color: black; */
+    }
 
-.swal2-actions .swal2-loader {
-    position: relative;
-}
+    .select-group-week .select-week select {
+        width: 100%;
+        height: 35px;
+        border-radius: 5px;
+        background-color: #3AA6B9;
+        color: white;
+        border: none;
+        outline: 0px;
+        font-size: 15px;
+        padding: 3px;
+        transition: all 0.6s;
+    }
 
-.swal2-container .swal2-styled.swal2-confirm {
-    border: 0;
-    border-radius: 0.25em;
-    /* background: initial; */
-    background-color: green !important;
-    color: #fff;
-    font-size: 1em;
-    padding: 10px 25px;
-    position: absolute;
-    right: 100px;
-}
+    .select-group-week .select-group select:hover,
+    .select-week select:hover {
+        cursor: pointer;
+        background-color: #0C356A;
+        transition: all 0.7s;
+    }
 
-.swal2-container .swal2-styled.swal2-cancel {
-    border: 0;
-    border-radius: 0.25em;
-    background: initial;
-    background-color: red !important;
-    color: #fff;
-    font-size: 1em;
-    padding: 10px 25px;
-    position: absolute;
-    left: 100px;
-}
+    /* @change-slot-time-height */
+    td.fc-timegrid-slot {
+        height: 37px !important;
+    }
 
-@media screen and (min-width: 1369px) {
+    .fc .fc-toolbar.fc-header-toolbar {
+        margin-bottom: 0.54em;
+    }
 
+    /* @turn-off-color-current-day */
+    .fc .fc-timegrid-col.fc-day-today {
+        background-color: white;
+    }
+
+    /* @color-11-12pm */
+    tbody tr:nth-child(9),
+    tbody tr:nth-child(10),
+    tbody tr:nth-child(11),
+    tbody tr:nth-child(12),
+    tbody tr:nth-child(21) {
+        background-color: #f4f4f4 !important;
+    }
+
+    /* @like-colespan on 11 & 12pm */
+    .fc .fc-non-business {
+        background-color: transparent;
+    }
+
+    a.fc-event,
+    a.fc-event {
+        background-color: #3AA6B9;
+        border: 1px solid #3AA6B9;
+    }
+
+    .fc-timegrid-event .fc-event-main {
+        padding: 0px;
+    }
     /* @event */
     .container-room {
         width: 100%;
         height: 100%;
         position: relative;
         background-color: #3AA6B9;
-        display: flex !important;
-        justify-content: space-between;
+        position: relative;
+    }
+    .container-room:hover .delete{
+        width: 13px;
+        height: 13px;
+        background-color: red;
+        border-top-right-radius: 3px;
+        color: white;
+        font-weight: bold;
+        position: absolute;
+        text-align: center;
+        justify-content: center;
+        z-index: 1;
+        right: 0;
+        top: 0;
+        font-size: 10px;
+        cursor: pointer;
+    }
+    .container-room:hover .sideCourse{
+        background-color: #FFF6E0 !important;
     }
 
     /* @course-section */
     .sideCourse {
-        width: 60%;
-        height: 100%;
+        width: 100%;
+        height: 63%;
         background-color: white;
-        border-top-right-radius: 0px;
-        border-bottom-left-radius: 3px;
-        padding: 4px 6px;
-        font-size: 12px;
-        font-weight: 600;
+        padding: 6px 6px;
+        font-size: 13px;
         position: absolute;
-        padding-top: 10px;
+        font-weight: 600;
+        border-top-left-radius: 3px;
+        border-top-right-radius: 3px;
     }
-
-    .courseName {
+    .courseName .courseNameText {
         font-size: 14px;
         color: black;
-        padding-bottom: 4px;
+        /* margin-bottom: 6px;
+            line-height: 1.3em;
+            max-height: calc(2 * 1.2em);
+            overflow: hidden;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;  */
     }
 
     .courseType {
-        padding-bottom: 4px;
+        padding-bottom: 6px;
         color: red;
-        padding-top: 3px;
     }
 
     .lecturer {
         color: green;
-        font-size: 14px;
-        padding-top: 3px;
     }
 
     /* @room-section */
     .sideRoom {
-        width: 40%;
-        height: 100%;
+        width: 100%;
+        height: 37%;
         background-color: #3AA6B9;
+        bottom: 0;
         position: absolute;
-        padding: 6px 5px;
+        padding: 6px 6px;
         font-weight: 500;
         font-size: 13px;
-        right: 0;
-        padding-top: 10px;
     }
 
     .roomName {
         padding-bottom: 4px;
     }
-}</style>
+    .delete h1{
+        width: 13px;
+        height: 13px;
+        display: none;
+        transition: all 0.5s;
+    }
+    .container-room:hover .delete{
+        width: 13px;
+        height: 13px;
+        background-color: red;
+        border-top-right-radius: 3px;
+        color: white;
+        font-weight: bold;
+        position: absolute;
+        text-align: center;
+        justify-content: center;
+        z-index: 1;
+        right: 0;
+        top: 0;
+        font-size: 10px;
+        cursor: pointer;
+        transition: all 0.5s;
+    }
+    /* 
+    .delete {
+        width: 13px;
+        height: 13px;
+        background-color: red;
+        border-top-right-radius: 3px;
+        color: white;
+        font-weight: bold;
+        position: absolute;
+        text-align: center;
+        justify-content: center;
+        z-index: 1;
+        right: 0;
+        top: 0;
+        font-size: 10px;
+        cursor: pointer;
+    } */
+
+    .swal2-container .swal2-popup {
+        width: 25rem;
+        height: 270px;
+        max-width: 100%;
+        border: none;
+        border-radius: 5px;
+        background: #fff;
+        color: #545454;
+        display: none;
+        padding: 0 0.1em 2.3em;
+        position: relative;
+        box-sizing: border-box;
+        grid-template-columns: minmax(0, 100%);
+        font-family: inherit;
+        font-size: 1rem;
+    }
+
+    .swal2-icon.swal2-warning {
+        border-color: red;
+        color: red;
+        margin-bottom: -25px;
+        /* font-size: 14px; */
+    }
+
+    .swal2-title {
+        /* font-size: 25px; */
+        color: black;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .swal2-actions .swal2-loader {
+        position: relative;
+    }
+
+    .swal2-container .swal2-styled.swal2-confirm {
+        border: 0;
+        border-radius: 0.25em;
+        /* background: initial; */
+        background-color: green !important;
+        color: #fff;
+        font-size: 1em;
+        padding: 10px 25px;
+        position: absolute;
+        right: 100px;
+    }
+
+    .swal2-container .swal2-styled.swal2-cancel {
+        border: 0;
+        border-radius: 0.25em;
+        background: initial;
+        background-color: red !important;
+        color: #fff;
+        font-size: 1em;
+        padding: 10px 25px;
+        position: absolute;
+        left: 100px;
+    }
+
+    @media screen and (min-width: 1369px) {
+
+        /* @event */
+        .container-room {
+            width: 100%;
+            height: 100%;
+            position: relative;
+            background-color: #3AA6B9;
+            display: flex !important;
+            justify-content: space-between;
+        }
+
+        /* @course-section */
+        .sideCourse {
+            width: 60%;
+            height: 100%;
+            background-color: white;
+            border-top-right-radius: 0px;
+            border-bottom-left-radius: 3px;
+            padding: 4px 6px;
+            font-size: 12px;
+            font-weight: 600;
+            position: absolute;
+            padding-top: 10px;
+        }
+
+        .courseName {
+            font-size: 14px;
+            color: black;
+            padding-bottom: 4px;
+        }
+
+        .courseType {
+            padding-bottom: 4px;
+            color: red;
+            padding-top: 3px;
+        }
+
+        .lecturer {
+            color: green;
+            font-size: 14px;
+            padding-top: 3px;
+        }
+
+        /* @room-section */
+        .sideRoom {
+            width: 40%;
+            height: 100%;
+            background-color: #3AA6B9;
+            position: absolute;
+            padding: 6px 5px;
+            font-weight: 500;
+            font-size: 13px;
+            right: 0;
+            padding-top: 10px;
+        }
+
+        .roomName {
+            padding-bottom: 4px;
+        }
+    }
+</style>
